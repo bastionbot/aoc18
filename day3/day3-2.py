0303b817ab221c1e0dd800bd9aaaed6d052e4332
bastionbot <droneship16@gmail.com>
2018-12-03 17:26:08 -0600

    day 3

---
+++ day3/day3-2.py
@@ -0,0 +1,39 @@
+lines = []
+table = []
+dq = []
+x,y,runx,runy,maxx,maxy = 0,0,0,0,0,0
+squares = 0
+with open('input.txt') as f:
+	for line in f.readlines():
+		lines.append(line.strip())
+
+for line in lines:
+	x = int(line.split(':')[0].split(' ')[2].split(',')[0])
+	y = int(line.split(':')[0].split(' ')[2].split(',')[1])
+	runx = int(line.split(':')[1].split(' ')[1].split('x')[0])
+	runy = int(line.split(':')[1].split(' ')[1].split('x')[1])
+	if maxx < x+runx:
+		maxx = x+runx
+	if maxy < y+runy:
+		maxy = y+runy
+
+table = [[0 for i in range(maxx)] for j in range(maxy)]
+
+for line in lines:
+	x = int(line.split(':')[0].split(' ')[2].split(',')[0])
+	y = int(line.split(':')[0].split(' ')[2].split(',')[1])
+	runx = int(line.split(':')[1].split(' ')[1].split('x')[0])
+	runy = int(line.split(':')[1].split(' ')[1].split('x')[1])
+	for i in range(y,runy+y):
+		for j in range(x,runx+x):
+			if table[i][j] != 0:
+				if line.split(' ')[0] in dq:
+					continue
+				dq.append(line.split(' ')[0])
+			elif table[i][j] == 0:
+				table[i][j] = line.split(' ')[0]
+for line in lines:
+	if line.split(' ')[0] in dq:
+		continue
+	print(line.split(' ')[0])
+print(dq)
