4ab37980f828b1bddadd33c93492d29cd82ca9c0
bastionbot <droneship16@gmail.com>
2018-12-04 18:21:21 -0600

    Almost there

--- day4/day4-1.py
+++ day4/day4-1.py
@@ -1,40 +1,62 @@
-import datetime, itertools
+import datetime
+from statistics import mode
 log = []
-guards = {}
+dates = []
+datetimes = []
+guardtimes = {}
+mergedlist = []
+guards = {} # logs all guards and when they sleep
+
 with open('input.txt') as f:
 	for line in f.readlines():
 		log.append(line.strip())
 
 sortedlog = { datetime.datetime.strptime(ts.split(']')[0].split('[')[1], '%Y-%m-%d %H:%M'): ts.split(']')[1].strip() for ts in log }
-guardlist = { date.date() : {} for i in range(0,60) for date in list(set(sortedlog.keys()))}
+for date in list(sortedlog.keys()):
+	dates.append(date.date())
+	datetimes.append(date)
+	guards[date.date()] = {}
+dates = sorted(set(dates))
+datetimes = sorted(datetimes)
 
-for item in sorted(sortedlog.keys()):
-	print(item)
-	if "Guard #" in sortedlog[item]:
-		guards[sortedlog[item].split('#')[1].split(' ')[0]] = []
-		currentguard = sortedlog[item].split('#')[1].split(' ')[0]
+guardlist = { date : {} for i in range(0,60) for date in dates }
+
+for date in datetimes:
+	if 'Guard #' in sortedlog[date]:
+		currentguard = sortedlog[date].split('#')[1].split(' ')[0]
+		guards[date.date()][currentguard] = []
+		guardlist[date.date()][currentguard] = []
 	else:
-		guards[currentguard].append(item)
-for guard in guards:
-	for i in range(len(guards[guard])):
-		try:
-			foo = guards[guard][i+1]
-			x = guards[guard][i]
-			y = guards[guard][i+1]
-			guardlist[x.date()][guard] = []
-		except:
-			break
-		guard_time, remainder = divmod((y-x).total_seconds(), 60)
-		for i in range(x.time().minute, y.time().minute):
-			guardlist[x.date()][guard].append(i)
+		if currentguard not in guards[date.date()].keys():
+			guards[date.date()][currentguard] = []
+		guards[date.date()][currentguard].append(date)
+		guardlist[date.date()][currentguard] = []
 
-for date in guardlist:
+for date in guards:
+	for guard in guards[date]:
+		for i in range(len(guards[date][guard])):
+			try:
+				x = guards[date][guard][i] # get start of sleep
+				y = guards[date][guard][i+1] # get end of sleep
+			except:
+				break
+			guard_time, remainder = divmod((y-x).total_seconds(), 60)
+			i+=1
+			for i in range(x.time().minute, y.time().minute):
+				guardlist[x.date()][guard].append(i)
+
+for date in dates:
+	total = 0
 	for guard in guardlist[date]:
-		print(guard, len(guardlist[date][guard]))
-
-for guard in guards:
-	for date in guardlist.keys():
-		try:
-			print(date, guard, guardlist[date][guard])
-		except:
-			continue
+		if guard not in guardtimes.keys():
+			guardtimes[guard] = 0
+		total += len(guardlist[date][guard])
+		guardtimes[guard] += total
+sleepy = max(guardtimes, key=guardtimes.get)
+print('Guard #'+ str(sleepy) +' slept the most.')
+for date in dates:
+	if sleepy in guardlist[date].keys():
+		mergedlist += guardlist[date][sleepy]
+asleep = max(set(mergedlist), key=mergedlist.count)
+print('Sleepy spent minute ' + str(asleep) + ' asleep the most.')
+print('Solution: ' + str(int(asleep)*int(sleepy)))
