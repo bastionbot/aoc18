3c0c883f71c62deefdcce17c632c9e1e2b28efb7
bastionbot <droneship16@gmail.com>
2018-12-08 16:50:08 -0600

    day 7 - 2 wip ;

--- day7/day7.py
+++ day7/day7.py
@@ -1,8 +1,12 @@
+#!/bin/python3
+import string, sys
+global step 
 step = ''
 reqs = {}
 steps = []
 presteps = []
-with open('input.txt') as f:
+workers = []
+with open(sys.argv[1]) as f:
 	for line in f.readlines():
 		p = line.strip().split(' ')[7]
 		s = line.strip().split(' ')[1]
@@ -16,16 +20,52 @@ with open('input.txt') as f:
 			reqs[p].append(s)
 for char in ''.join(sorted(list(set(steps) - set(presteps)))):
 	reqs[char] = []
-while (len(step) < len(steps)+1):
+
+def valid_step(reqs):
+	foo = None
 	for k in sorted([j for j,v in reqs.items() if not v]):
 		try:
 			if k not in step and k == min(list(set(''.join([j for j,v in reqs.items() if not v])) - set(step))):
-				step += k
-				for l in reqs.keys():
-					try:
-						reqs[l].remove(k)
-					except:
-						continue
+				foo = k
 		except:
 			continue
-print(''.join(step))
+	return foo
+def remove_step(reqs, k):
+	global step
+	for l in reqs.keys():
+		try:
+			reqs[l].remove(k)
+		except:
+			continue
+	step += k
+	return reqs, step
+class worker:
+	def __init__(self):
+		self.status = 'Idle'
+		self.starttime = 0
+		self.step = '.'
+timer = 0
+for i in range(int(sys.argv[2])):
+	workers.append(worker())
+info = ("Second", '\t'.join(['Elf'+str(i) for i in range(int(sys.argv[2]))]), "Step")
+while (len(step) < len(steps)+1):
+	for worker in workers:
+		if worker.status == 'Working':
+			if worker.step is not None:
+				if (string.ascii_uppercase.index(worker.step) + 1) + worker.starttime == timer:
+					worker.status = 'Idle'
+					worker.starttime = 0
+					reqs, step = remove_step(reqs, worker.step)
+					worker.step = '.'
+		elif worker.status == 'Idle' and valid_step(reqs) not in [work.step for work in workers] and valid_step(reqs) is not None:
+			worker.step = valid_step(reqs)
+			worker.status = 'Working'
+			worker.starttime = timer
+		else:
+			worker.status = 'Idle'
+	if valid_step(reqs) not in [work.step for work in workers] and valid_step(reqs) is not None:
+		continue
+	timer += 1
+	print('%s\t%s\t%s' % info)
+	info = (str(timer), '\t'.join([worker.step for worker in workers]), step)
+print("Total time: %d\nFinal step: %s" % (timer, ''.join(step)))
